9a882e91582361d07298e002c00cb3b3166fd079
Christian Hofstaedtler <zeha@debian.org>
2017-10-10 15:04:34 -0300

    rdoc: build reproducible documentation
    - sort input filenames
    - provide a fixed timestamp to the gzip compression
    
    Signed-off-by: Antonio Terceiro <terceiro@debian.org>
    Signed-off-by: Christian Hofstaedtler <zeha@debian.org>
    
    Gbp-Pq: Name rdoc-build-reproducible-documentation.patch

--- lib/rdoc/generator/json_index.rb
+++ lib/rdoc/generator/json_index.rb
@@ -176,7 +176,7 @@ class RDoc::Generator::JsonIndex
     debug_msg "Writing gzipped search index to %s" % outfile
 
     Zlib::GzipWriter.open(outfile) do |gz|
-      gz.mtime = File.mtime(search_index_file)
+      gz.mtime = -1
       gz.orig_name = search_index_file.basename.to_s
       gz.write search_index
       gz.close
@@ -194,7 +194,7 @@ class RDoc::Generator::JsonIndex
         debug_msg "Writing gzipped file to %s" % outfile
 
         Zlib::GzipWriter.open(outfile) do |gz|
-          gz.mtime = File.mtime(dest)
+          gz.mtime = -1
           gz.orig_name = dest.basename.to_s
           gz.write data
           gz.close
