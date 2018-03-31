49a95f7b1a1c03ff1cf3c50254532f8b65b74938
Antonio Terceiro <terceiro@debian.org>
2018-03-31 12:54:51 -0300

    debian/tests/excludes/any/TestTimeTZ.rb: ignore tests failing due to assumptions that don't hold on newer tzdata update. Upstream bug: https://bugs.ruby-lang.org/issues/14655
    Origin: backported, b18e5cbfd4e08ed84054b91fd0d75132c97158c8
    Reviewed-by: Santiago R.R. <santiagorr@riseup.net>
    Signed-off-by: Santiago R.R <santiagorr@riseup.net>

---
+++ test/excludes/TestTimeTZ.rb
@@ -0,0 +1,4 @@
+exclude :test_gen_Pacific_Kiritimati_71, "https://bugs.ruby-lang.org/issues/14655"
+exclude :test_gen_Pacific_Kiritimati_89, "https://bugs.ruby-lang.org/issues/14655"
+exclude :test_gen_lisbon_99, "https://bugs.ruby-lang.org/issues/14655"
+exclude :test_pacific_kiritimati, "https://bugs.ruby-lang.org/issues/14655"
