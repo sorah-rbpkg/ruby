d446f781cdec969be227f851b84fadbe6281eb17
Santiago R.R <santiagorr@riseup.net>
2018-05-15 13:11:28 +0200

    remove test/excludes/TestRinda.rb gargabe file
    Gbp-Dch: Ignore
    Signed-off-by: Santiago R.R <santiagorr@riseup.net>

--- test/excludes/TestRinda.rb
+++
@@ -1,5 +0,0 @@
-reason = "Network access not allowed during build in Debian"
-exclude :test_make_socket_ipv4_multicast, reason
-exclude :test_make_socket_ipv4_multicast_hops, reason
-exclude :test_make_socket_ipv4_multicast, reason
-exclude :test_ring_server_ipv4_multicast, reason
