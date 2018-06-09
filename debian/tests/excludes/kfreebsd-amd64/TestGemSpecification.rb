40972dba1f70422621cc1d9d4b2559cf7650c13c
Svante Signell <svante.signell@gmail.com>
2018-06-09 10:22:43 -0300

    Exclude tests that fail on kfreebsd
    Closes: #899267
    
    Signed-off-by: Antonio Terceiro <terceiro@debian.org>
    Signed-off-by: Svante Signell <svante.signell@gmail.com>

---
+++ debian/tests/excludes/kfreebsd-amd64/TestGemSpecification.rb
@@ -0,0 +1,2 @@
+# Actual is one day earlier than Expected
+exclude :test_date_equals_time, 'fails on kFreeBSD'
---
+++ debian/tests/excludes/kfreebsd-i386/TestRubyOptions.rb
@@ -0,0 +1,3 @@
+# Output format of ps is different?
+exclude :test_set_program_name, 'fails on kFreeBSD'
+exclude :test_setproctitle, 'fails on kFreeBSD'
