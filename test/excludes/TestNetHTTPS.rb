0d4a0a833c2c6b3f3b86dbc03671ed4897a8ef94
Sorah Fukumori <sorah@cookpad.com>
2019-07-13 12:29:34 +0900

    TestNetHTTPS: exclude test_session_reuse*

---
+++ test/excludes/TestNetHTTPS.rb
@@ -0,0 +1,2 @@
+exclude :test_session_reuse, "broken with OpenSSL 1.1.x in 2.4 https://github.com/ruby/openssl/issues/207"
+exclude :test_session_reuse_but_expire, "broken with OpenSSL 1.1.x in 2.4 https://github.com/ruby/openssl/issues/207"
