e0e4d0c57b9320dbafc85ce358fb28139ed8d0e8
Sorah Fukumori <sorah@cookpad.com>
2019-07-13 01:23:27 +0000

    New upstream version 2.4.6

--- eval.c
+++ eval.c
@@ -2,7 +2,7 @@
 
   eval.c -
 
-  $Author: nagachika $
+  $Author: usa $
   created at: Thu Jun 10 14:22:17 JST 1993
 
   Copyright (C) 1993-2007 Yukihiro Matsumoto
@@ -173,6 +173,7 @@ ruby_cleanup(volatile int ex)
 
       step_0: step++;
 	errs[1] = th->errinfo;
+	if (THROW_DATA_P(th->errinfo)) th->errinfo = Qnil;
 	th->safe_level = 0;
 	ruby_init_stack(&errs[STACK_UPPER(errs, 0, 1)]);
 
