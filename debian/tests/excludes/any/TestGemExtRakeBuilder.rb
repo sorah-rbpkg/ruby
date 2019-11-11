e2b4d6e7adb73b8bfb5d64c9ec0f2f36c94324e1
Lucas Kanashiro <kanashiro@debian.org>
2019-11-11 16:09:59 -0300

    Exclude tests which fail due to ~ in version string
    Those tests should be included again when upstream releases a new
    version and the ~ become not neeeded.

---
+++ debian/tests/excludes/any/TestGemExtRakeBuilder.rb
@@ -0,0 +1,3 @@
+exclude :test_class_build_no_mkrf_passes_args,  "tries to skip ~ in version string"
+exclude :test_class_build,  "tries to skip ~ in version string"
+exclude :test_class_build_with_args,  "tries to skip ~ in version string"
