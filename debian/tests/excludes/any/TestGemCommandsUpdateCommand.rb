edc664194eb97429cbcc16a5658fb11f36b3eacf
Lucas Kanashiro <lucas.kanashiro@canonical.com>
2021-09-13 19:45:30 -0300

    Skip tests which require to update gems in the system
    `gem update --system` was disabled in rubygems-integration. Those tests
    keep failing because they attempt to use that.

---
+++ debian/tests/excludes/any/TestGemCommandsUpdateCommand.rb
@@ -0,0 +1,3 @@
+exclude :test_execute_system_at_latest, "gem update --system was disabled by rubygems-integration"
+exclude :test_execute_system_specific_older_than_minimum_supported_rubygems, "gem update --system was disabled by rubygems-integration"
+exclude :test_execute_system_with_gems, "gem update --system was disabled by rubygems-integration"
