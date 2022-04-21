865da63375a11b0bdab45d6ff90f8a211dae1256
Antonio Terceiro <terceiro@debian.org>
2022-04-21 19:12:57 -0300

    Exclude new tests for "gem update --system"

--- debian/tests/excludes/any/TestGemCommandsUpdateCommand.rb
+++ debian/tests/excludes/any/TestGemCommandsUpdateCommand.rb
@@ -8,3 +8,5 @@ exclude :test_execute_system_specific, "gem update --system was disabled by ruby
 exclude :test_execute_system_specific_newer_than_or_equal_to_3_2_leaves_plugins_dir_alone, "gem update --system was disabled by rubygems-integration"
 exclude :test_execute_system_specific_older_than_3_2_removes_plugins_dir, "gem update --system was disabled by rubygems-integration"
 exclude :test_execute_system_specifically_to_latest_version, "gem update --system was disabled by rubygems-integration"
+exclude :test_execute_system_when_latest_does_not_support_your_ruby, "gem update --system was disabled by rubygems-integration"
+exclude :test_execute_system_update_installed, "gem update --system was disabled by rubygems-integration"
