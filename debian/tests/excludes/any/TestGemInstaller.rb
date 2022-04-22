cb7f11709c6859c76d29834b7ab4fe407280c97d
Antonio Terceiro <terceiro@debian.org>
2022-04-22 09:58:26 -0300

    Exclude test TestGemInstaller#test_ensure_no_race_conditions_between_installing_and_loading_gemspecs
    This test seems to fail random (what makes sense given its name).

--- debian/tests/excludes/any/TestGemInstaller.rb
+++ debian/tests/excludes/any/TestGemInstaller.rb
@@ -1,2 +1,3 @@
 exclude :test_generate_plugins, "requires building gem native extensions"
 exclude :test_keeps_plugins_up_to_date, "requires building gem native extensions"
+exclude :test_ensure_no_race_conditions_between_installing_and_loading_gemspecs, "seems to fail randomly"
