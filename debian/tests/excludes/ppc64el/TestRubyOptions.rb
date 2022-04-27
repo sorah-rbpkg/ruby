906cd0e7a56193ef7f9bce92c9b947b6bc6eb712
Antonio Terceiro <terceiro@debian.org>
2022-04-27 13:00:21 -0300

    Drop ppc64el-specific test exclusions
    The bugs have been fixed since those were added.

--- debian/tests/excludes/ppc64el/TestRubyOptions.rb
+++
@@ -1,3 +0,0 @@
-exclude :test_segv_loaded_features, 'fails, ENOTIME to investigate'
-exclude :test_segv_setproctitle, 'fails, ENOTIME to investigate'
-exclude :test_segv_test, 'fails, ENOTIME to investigate'
